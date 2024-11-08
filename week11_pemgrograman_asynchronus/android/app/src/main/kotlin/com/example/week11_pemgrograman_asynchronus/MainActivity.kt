c67f10e6af9d81d1632bc1049d34a8dab0f28004
rafsanalhad <92444864+rafsanalhad@users.noreply.github.com>
2024-11-08 08:31:00 +0700

    W12: Soal 3, 4, 6 (Praktikum 1 Praktikum 2 Praktium 3)

---
+++ week11_pemgrograman_asynchronus/android/app/src/main/kotlin/com/example/week11_pemgrograman_asynchronus/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.week11_pemgrograman_asynchronus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ week11_pemgrograman_asynchronus/lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const FuturePage(),
+    );
+  }
+}
+
+class FuturePage extends StatefulWidget {
+  const FuturePage({super.key});
+
+  @override
+  State<FuturePage> createState() => _FuturePageState();
+}
+
+class _FuturePageState extends State<FuturePage> {
+  Future<http.Response> getData() async {
+    const authority = 'www.googleapis.com';
+    const path = 'books/v1/volumes/junbDwAAQBAJ';
+    Uri url = Uri.https(authority, path);
+    return await http.get(url);
+  }
+
+  Future<int> returnOneAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 1;
+  }
+
+  Future<int> returnTwoAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 2;
+  }
+
+  Future<int> returnThreeAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 3;
+  }
+
+  Future count() async {
+    int total = 0;
+    total = await returnOneAsync();
+    total += await returnTwoAsync();
+    total += await returnThreeAsync();
+    setState(() {
+      result = total.toString();
+    });
+  }
+
+  late Completer<int> completer;
+
+  Future<int> getNumber() {
+    completer = Completer<int>();
+    calculate();
+    return completer.future;
+  }
+
+  Future<void> calculate() async {
+    try{
+    await Future.delayed(const Duration(seconds: 5));
+    completer.complete(42);
+    } catch(e){
+      result = 'An error occured: $e';
+    }
+  }
+
+  String result = '';
+  bool isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Back From The Future'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // ElevatedButton(
+            //   child: const Text('GO!'),
+            //   onPressed: () {
+            //     setState(() {
+            //       isLoading = true;
+            //     });
+            //     getData().then((value) {
+            //       setState(() {
+            //         result = value.body.toString().substring(0, 450);
+            //         isLoading = false;
+            //       });
+            //     }).catchError((error) {
+            //       setState(() {
+            //         result = 'Error: $error';
+            //         isLoading = false; // Selesai loading
+            //       });
+            //     });
+            //   },
+            // ),
+            ElevatedButton(
+                child: Text('GO!'),
+                onPressed: () {
+                  getNumber().then((value){
+                    setState((){
+                      result = value.toString();
+                    });
+                  });
+                }),
+            const SizedBox(height: 20),
+            if (isLoading)
+              const CircularProgressIndicator()
+            else
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  result,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
