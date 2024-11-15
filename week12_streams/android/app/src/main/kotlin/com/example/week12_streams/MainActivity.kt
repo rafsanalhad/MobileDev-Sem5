8942c2bf46c7a8f600580ddb3fa55ae9529e96be
rafsanalhad <92444864+rafsanalhad@users.noreply.github.com>
2024-11-15 08:06:15 +0700

    W13: Jawaban Soal 4

---
+++ week12_streams/android/app/src/main/kotlin/com/example/week12_streams/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.week12_streams
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ week12_streams/lib/stream.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ColorStream {
+  final List<Color> colors = [
+    Colors.blueGrey,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal
+  ];
+
+  Stream<Color> get colorStream async* {
+    yield* Stream.periodic(
+      const Duration(seconds: 1),
+      (int t) {
+        int index = t % colors.length;
+        return colors[index];
+      },
+    );
+  }
+}
